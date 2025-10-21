2487085d6d7ecff9627d936dbe15dd551b45d6af
thomil13 <thomas.milde13@gmail.com>
2025-10-21 12:43:51 +0100

    Add FIre Control Input, set up JS structure

---
+++ assets/js/scripts-tca.js
@@ -0,0 +1,11 @@
+// Three Classes Needed
+
+// 1 - Model (Game Logic)
+
+// 2 - View (UI Messages)
+
+// 3 - Controller (Inputs and Game Management)
+
+// Helper Functions & Event Handlers
+
+// Init Function
